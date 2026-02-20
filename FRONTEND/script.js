81a4c730ccf97c3a9c42683f18989cc19e6853cb
Snakestongue <maeher.grover@gmail.com>
2026-02-19 21:16:53 -0500

    Style + Mobile Updates

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -181,84 +181,115 @@ function newKeyboard(){
     let bottomDiv = document.getElementById("buttons")
     textbox.innerHTML = ""
     let main = document.getElementsByTagName("main")[0];
-    document.addEventListener("keydown", function(key){
-            if (key.key =="Backspace") {
+    let keyboard = document.getElementById("keyboard");
+    let shiftOn = false;
+
+    document.addEventListener("keydown", function(event) {
+    if (event.key == " ") {
+        event.preventDefault(); //should stop space jumping
+        textbox.innerHTML += " ";
+    } else if (event.key == "Backspace") {
         textbox.innerHTML = textbox.innerHTML.slice(0, -1);
-    }
-    if (key.key == "Enter") {
-        let enterBreak = document.createElement("br")
-        textbox.appendChild(enterBreak);
-    }
-    if (key.key.length === 1) {
+    } else if (event.key == "Enter") {
+        textbox.innerHTML += "<br>";
+    } else if (event.key.length == 1) {
         textbox.innerHTML += event.key;
     }
-    })
-
-    let resetB = document.createElement("button");
-    resetB.style.backgroundColor = "black";
-    resetB.style.color = "white";
-    resetB.style.width = "90px"
-    resetB.innerHTML = ("reset");
-    resetB.addEventListener("click", function(){
-         textbox.innerHTML = " "
-    })
-    bottomDiv.appendChild(resetB);
-    let keyboard = document.getElementById("keyboard");
-    
-
-    //shift
-    let shiftOn = false;
-    let shift = document.createElement("button");
-    shift.innerHTML = "shift"
-    shift.style.color = "white"
-        shift.style.backgroundColor = "black"
-        shift.style.width = "50px"
-    shift.style.border = "1px solid white";
-    shift.addEventListener("click", function(){
-        
-        shiftOn = true;
-    })
+    });
     
     //a-z + space 
-    for (let i =0; i<alpha.length; i++){
+    for (let i=0; i<alpha.length; i++){
         let create = document.createElement("button");
-        
         create.innerHTML = alpha[i];
+
+       // create.style.margin = "2px 10px";
         create.style.color = "white"
         create.style.backgroundColor = "black"
         create.style.width = "35px"
         create.style.height = "25px"
         create.style.border = "1px solid white";
-        if (alpha[i] == "z" || alpha[i]=="q" || alpha[i]=="a" || alpha[i] == ' '){
+        
+       
+        if (alpha[i] == "q" || alpha[i] == "a" || alpha[i] == "z"){
             let br = document.createElement("br");
             keyboard.appendChild(br);
         }
-        
         if (alpha[i] == ' '){
-                create.innerHTML = ("space");
-                create.style.color = "white"
-                create.style.backgroundColor = "black"
-                create.style.width = "50px"
-                create.style.border = "1px solid white";
+            let br = document.createElement("br");
+            keyboard.appendChild(br);
+            create.innerHTML = "space";
+            create.style.width = "200px";
+            create.style.height = "19px"
+                // create.innerHTML = ("space");
+                // create.style.color = "white"
+                // create.style.backgroundColor = "black"
+                // create.style.width = "60px"
+                // create.style.border = "1px solid white";
                 }
+        create.addEventListener("mousedown", function() {
+            this.style.opacity = "0.5";
+        });
+        create.addEventListener("mouseup", function() {
+            this.style.opacity = "1";
+        });
+        create.addEventListener("mouseleave", function() {
+            this.style.opacity = "1";
+        });
         create.addEventListener("click", function(){
+           
             if (shiftOn == true){
                 textbox.innerHTML +=capitals[i];
                 shiftOn =false;
             }else{
             textbox.innerHTML += alpha[i];
-            }
+        }
         });
         keyboard.appendChild(create);
     }
+//extras
+
+    //reset button
+    let resetB = document.createElement("button");
+    resetB.innerHTML = "reset"
+    resetB.style.margin = "2px"
+    resetB.style.color = "white"
+    resetB.style.backgroundColor = "black"
+    resetB.style.width = "80px"
+    resetB.style.border = "1px solid white";
+    resetB.style.cursor = "pointer"
+
+    resetB.addEventListener("click", function(){
+         textbox.innerHTML = ""
+    })
     
+
+    //shift
+    let shift = document.createElement("button");
+    shift.innerHTML = "shift"
+
+    shift.style.margin = "2px"
+    shift.style.color = "white"
+    shift.style.backgroundColor = "black"
+    shift.style.width = "80px"
+    shift.style.border = "1px solid white";
+    shift.style.cursor = "pointer"
+
+    shift.addEventListener("click", function(){
+        shiftOn = true;
+        
+    })
+
     //backSpace
     let backSpace = document.createElement("button");
     backSpace.innerHTML = "backspace"
+
+    backSpace.style.margin = "2px"
     backSpace.style.color = "white"
-        backSpace.style.backgroundColor = "black"
-        backSpace.style.width = "80px"
-        backSpace.style.border = "1px solid white";
+    backSpace.style.backgroundColor = "black"
+    backSpace.style.width = "80px"
+    backSpace.style.border = "1px solid white";
+    backSpace.style.cursor = "pointer"
+
     backSpace.addEventListener("click", function(){
         textbox.innerHTML = textbox.innerHTML.slice(0, -1);
  
@@ -267,19 +298,52 @@ function newKeyboard(){
     //enter
     let enterB = document.createElement("button");
     enterB.innerHTML = "enter";
-    enterB.style.backgroundColor = "black"
-        enterB.style.width = "50px"
+
+    enterB.style.margin = "2px"
     enterB.style.color = "white"
-        enterB.style.border = "1px solid white";
+    enterB.style.backgroundColor = "black"
+    enterB.style.width = "80px"
+    enterB.style.border = "1px solid white";
+    enterB.style.cursor = "pointer"
+
     enterB.addEventListener("click", function(){
-        let enterBreak = document.createElement("br")
+        let enterBreak = document.createElement("br");
         textbox.appendChild(enterBreak);
     })
+
+    enterB.addEventListener("mousedown", function() {
+            this.style.opacity = "0.5";
+        });
+    enterB.addEventListener("mouseup", function() {
+        this.style.opacity = "1";
+    });
+    enterB.addEventListener("mouseleave", function() {
+        this.style.opacity = "1";
+    });
+    backSpace.addEventListener("mousedown", function() {
+            this.style.opacity = "0.5";
+        });
+    backSpace.addEventListener("mouseup", function() {
+        this.style.opacity = "1";
+    });
+    backSpace.addEventListener("mouseleave", function() {
+        this.style.opacity = "1";
+    });
+    shift.addEventListener("mousedown", function() {
+            this.style.opacity = "0.5";
+        });
+    shift.addEventListener("mouseup", function() {
+        this.style.opacity = "1";
+    });
+    shift.addEventListener("mouseleave", function() {
+        this.style.opacity = "1";
+    });
+    
     
     
     keyboard.appendChild(shift);
     keyboard.appendChild(enterB);
     keyboard.appendChild(backSpace);
-    //keyboard.appendChild(resetB);
+    keyboard.appendChild(resetB);
 }
     
